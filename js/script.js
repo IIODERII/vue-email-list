f771e806c2cb92acdb86c76603fa66796fa608d9
IIODERII <oderrisi@gmail.com>
2023-11-07 13:49:55 +0100

    added link in variables

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ createApp({
   data() {
     return {
       emails: [],
+      baseAPI: "https://flynn.boolean.careers/exercises/api",
+      finalAPI: "/random/mail",
     };
   },
   methods: {},
   mounted() {
     for (let i = 0; i < 10; i++) {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((response) => {
-          this.emails.push(response.data.response);
-        });
+      axios.get(`${this.baseAPI}${this.finalAPI}`).then((response) => {
+        this.emails.push(response.data.response);
+      });
     }
   },
 }).mount("#app");
